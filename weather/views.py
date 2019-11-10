dd9f015446c811021e019f5ff0b645b76fb87b8d
alxayeed <alxayeed@gmail.com>
2019-11-10 12:54:16 +0600

    city list in json format is downloaded.API key for openweather.org is implemented

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,12 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=c979f3688d33f4b3c0d75df677d7d340'
+	city = 'Gazipur'
+	r = requests.get(url.format(city)).json()
+	print(r)
+	# print(r['weather'][0]['main'])
 
 	return render(request,'weather/index.html')
