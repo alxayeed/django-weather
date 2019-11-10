731baa2b5911bb83326e3392af57f25db2ab7934
alxayeed <alxayeed@gmail.com>
2019-11-10 15:31:07 +0600

    user can now search city and get weather infromation

--- weather/views.py
+++ weather/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=c979f3688d33f4b3c0d75df677d7d340'
-	city = 'London'
+	city = request.POST.get('city')
 	r = requests.get(url.format(city)).json()
 	
 
