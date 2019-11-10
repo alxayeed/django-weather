1dc9f399f33855b48c6580c98b243d6d96d941e9
alxayeed <alxayeed@gmail.com>
2019-11-10 20:07:31 +0600

    static file added.template redesigned.landing page,valid city,invalid city is working

--- weather/views.py
+++ weather/views.py
@@ -6,27 +6,30 @@ def index(request):
 	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=c979f3688d33f4b3c0d75df677d7d340'
 	city = request.POST.get('city')
 	r = requests.get(url.format(city)).json()
-	print(r)
-	
-	try:
+	if city == None :
 		city_weather = {
-			'cod':r['cod'],
-			'city':city,
-			'icon':r['weather'][0]['icon'],
-			'temperature':r['main']['temp'],
-			'description':r['weather'][0]['description'],
-			'Wind':r['wind']['speed'],
-			'Pressure':r['main']['pressure'],
-			'Humidity':r['main']['humidity'],
-			'Sunrise':r['sys']['sunrise'],
-			'Sunset':r['sys']['sunset'],
-			'Lon':r['coord']['lon'],
-			'Lat':r['coord']['lat']
-			
-			}
-	except KeyError:
-		city_weather = {
-		'cod': r['cod']
+		'name':'index'
 		}
-
+	else :
+	
+		try :
+			city_weather = {
+				'name':['name'],
+				'cod':r['cod'],
+				'city':city,
+				'icon':r['weather'][0]['icon'],
+				'temperature':r['main']['temp'],
+				'description':r['weather'][0]['description'],
+				'Wind':r['wind']['speed'],
+				'Pressure':r['main']['pressure'],
+				'Humidity':r['main']['humidity'],
+				'Sunrise':r['sys']['sunrise'],
+				'Sunset':r['sys']['sunset'],
+				'Lon':r['coord']['lon'],
+				'Lat':r['coord']['lat'],
+					}
+		except KeyError :                                                             
+			city_weather = {
+			'cod':r['cod']
+			}
 	return render(request,'weather/index.html',{'city_weather':city_weather})
